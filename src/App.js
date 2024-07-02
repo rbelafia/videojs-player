7da5104dc2ff15ccb8213c92807384ca5e270d79
Romain Belafia <romain.belafia@broadpeak.tv>
2024-07-02 14:53:39 +0200

    Set up player

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, {useRef, useState} from 'react';
+import VideoPlayer from './VideoPlayer';
 import './App.css';
+import videojs from "video.js";
 
-function App() {
+const App = () => {
+    const [urlChanging, setUrlChanging] = useState('');
+    const [url, setUrl] = useState('');
+    const playerRef = useRef(null);
+    const videoJsOptions = {
+        autoplay: false,
+        controls: true,
+        sources: [
+            {
+                src: url,
+            },
+        ],
+    };
+    const handlePlayerReady = (player) => {
+        playerRef.current = player;
+
+        // You can handle player events here, for example:
+        player.on('waiting', () => {
+            videojs.log('player is waiting');
+        });
+
+        player.on('dispose', () => {
+            videojs.log('player will dispose');
+        });
+    };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <h1>Video.js Player</h1>
+          <form onSubmit={(e) => {
+              e.preventDefault();
+              setUrl(urlChanging);
+          }}>
+              <label className="App-label">
+                  Url
+                  <input type={"url"} value={urlChanging} onChange={({target: {value}}) => {
+                      setUrlChanging(value)
+                  }}/>
+              </label>
+
+          </form>
+          {url !== '' && <VideoPlayer options={videoJsOptions} onReady={handlePlayerReady}/>}
+      </div>
   );
-}
+};
 
 export default App;
